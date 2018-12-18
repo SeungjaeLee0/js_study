0fe60d8dc2775956d93e12696ab5dc61a09e339c
jay <jaylee0830@gmail.com>
2018-12-18 16:39:18 +0900

    comments

--- groupByTest.js
+++ groupByTest.js
@@ -12,11 +12,20 @@ const max = function max(list){
 }
 
 function groupBy(data, arr, target, func){
+  //해당 property 값들 중 truthy한 값들만 가져오기 
   const props = data.map(data => arr.map(prop => data[prop]).filter(Boolean))
+
+  //가져온 property 값들 배열의 값으로 넣기
   const concatProps = props.map(item => item.reduce((acc, cur) => acc+','+cur))
+
+  //중복제거
   const uniqueProps = concatProps.filter((item, idx, arr) => arr.indexOf(item) === idx)
-  const result = uniqueProps.map(uniqueProp => ( { [md5(uniqueProp)] : uniqueProp, "data" :  ({ "user" : flat_data.filter( data => arr.map(props => data[props]).filter(Boolean).reduce((acc, cur) => acc+','+cur) === uniqueProp)
+
+  //결과
+  const result = uniqueProps.map(uniqueProp => ( { [md5(uniqueProp)] : uniqueProp
+  ,"data" :  ({ "user" : flat_data.filter( data => arr.map(props => data[props]).filter(Boolean).reduce((acc, cur) => acc+','+cur) === uniqueProp)
   ,[func.name+'-'+target] : func( (flat_data.filter( data => arr.map(props => data[props]).filter(Boolean).reduce((acc, cur) => acc+','+cur) === uniqueProp).map(data => data[target]).filter(Boolean)) ) })  } ) )
+
   return result
 }
 
