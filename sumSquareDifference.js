0235107c677063b9f8377dd046f5b2aad91da634
jay <jaylee0830@gmail.com>
2018-12-26 13:51:28 +0900

    Project Euler

---
+++ sumSquareDifference.js
@@ -0,0 +1,12 @@
+function sumSquareDifference(n) {
+    const range = (start, stop, step) => Array.from({ length: (stop - start + 1) / step }, (_, i) => start + (i * step));
+    const numbers = range(1, n, 1);
+    const sumOfSquares = numbers.map(number => number*number).reduce((acc, cur) => acc + cur);
+    const sum = numbers.reduce((acc, cur) => acc + cur);
+    const squareOfSum = sum * sum;
+    const result = squareOfSum - sumOfSquares;
+    // Good luck!
+    return result;
+  }
+  
+sumSquareDifference(100);
